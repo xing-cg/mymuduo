84b344a9417544131535f4ddfa82b62d9d523ebf
xcg <1933966629@qq.com>
2022-06-01 20:50:13 +0800

    add thread class, with c++11 thread-lib and Linux's sem

--- eventloop.cc
+++ eventloop.cc
@@ -57,7 +57,7 @@ void EventLoop::handleRead()
     ssize_t n = read(m_wakeupFd, &one, sizeof one);
     if(n != sizeof one)
     {
-        LOG_ERROR("%s reads %d bytes instead of 8", __FUNCTION__, n);
+        LOG_ERROR("%s reads %lu bytes instead of 8", __FUNCTION__, n);
     }
 }
 EventLoop::~EventLoop()
@@ -157,7 +157,7 @@ void EventLoop::wakeup()
     ssize_t n = write(m_wakeupFd, &one, sizeof one);
     if(n != sizeof one)
     {
-        LOG_ERROR("%s writes %d bytes instead of 8", __FUNCTION__, n);
+        LOG_ERROR("%s writes %lu bytes instead of 8", __FUNCTION__, n);
     }
 }
 void EventLoop::doPendingFunctors()
