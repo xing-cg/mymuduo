5e70d9d6beacde8bb4b72726a140d0b0356ca89c
xing-cg <xing-cg@qq.com>
2022-06-02 17:06:25 +0800

    add some notes on eventloopthreadpool.h

--- eventloopthreadpool.h
+++ eventloopthreadpool.h
@@ -11,19 +11,31 @@ public:
     using ThreadInitCallback = std::function<void(EventLoop*)>;
     EventLoopThreadPool(EventLoop *baseLoop, const std::string &nameArg);
     ~EventLoopThreadPool();
+public:
+    /* 供TcpServer调用 */
     void setThreadNum(int numThreads){m_numThreads = numThreads;}
+public:
+    /* 开启事件循环线程 */
     void start(const ThreadInitCallback &cb = ThreadInitCallback());
-    /* 如果在多线程中，m_baseLoop默认以轮询的方式分配channel给subloop */
-    EventLoop* getNextLoop();
+public:
+    /* 如果工作在多线程中，baseLoop默认以轮询的方式分配channel给subLoop */
+    EventLoop * getNextLoop();
+public:
+    /* 获取所有管理着的loop，存到vector中，相当于拷贝了m_loops */
     std::vector<EventLoop*> getAllLoops();
+public:
     bool started() const {return m_started;}
-    const std::string name() const{return m_name;}
+    const std::string name() const {return m_name;}
 private:
+    /* 用户最开始创建的loop */
     EventLoop * m_baseLoop;
     std::string m_name;
+    /* 包含了所有创建的线程 */
+    std::vector<std::unique_ptr<EventLoopThread>> m_threads;
+    /* 包含了所有管理着的loop的指针: 通过m_threads中的某个thread进行startLoop()返回loop的指针 */
+    std::vector<EventLoop*> m_loops;
+
     bool m_started;
     int m_numThreads;
     int m_next;
-    std::vector<std::unique_ptr<EventLoopThread>> m_threads;
-    std::vector<EventLoop*> m_loops;
 };
